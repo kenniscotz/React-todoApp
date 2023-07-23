7cba2adf6a649cede484901e206e3f2734f5c93f
kenni <kenniscotzotti@gmail.com>
2023-07-23 21:49:09 +0100

    frist react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useId } from "react";
+import "./App.css";
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const id = useId();
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your list items here"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul key={id}>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
-  );
-}
 
-export default App;
+    // It renders an input field and a button to add new todos. When the button is clicked,
+    // the new todo is added to the list and the input field is cleared.
+  );
+};
+
+export default TodoList;
